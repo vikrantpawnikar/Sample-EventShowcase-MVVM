6f6ebcb99b171f0a166a9c5361dc636ca842da0e
vikrant <vikrant5488@gmail.com>
2021-10-19 12:48:51 +0100

    Test case added for datasource

--- event/src/test/java/com/vikrant/event/eventList/datasource/EventListRemoteDataSourceTest.kt
+++ event/src/test/java/com/vikrant/event/eventList/datasource/EventListRemoteDataSourceTest.kt
@@ -1,7 +1,17 @@
 package com.vikrant.event.eventList.datasource
 
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import com.vikrant.core.model.Events
 import com.vikrant.core.retrofitclient.ClientInterface
+import com.vikrant.core.retrofitclient.ResultState
+import com.vikrant.event.BuildConfig
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
@@ -11,6 +21,9 @@ class EventListRemoteDataSourceTest {
     private lateinit var eventListRemoteDataSource: EventListDataSource
 
     @Mock
+    private val mockEvents = mock<Events>()
+
+    @Mock
     lateinit var api: ClientInterface
 
     @Before
@@ -18,4 +31,26 @@ class EventListRemoteDataSourceTest {
         eventListRemoteDataSource = EventListRemoteDataSource(api)
     }
 
+    @ExperimentalCoroutinesApi
+    @Test
+    fun testSuccessApiResponse() = runBlocking {
+        //given
+        whenever(api.getEvents(BuildConfig.API_KEY)).thenReturn(mockEvents)
+        //when
+        val emits  = eventListRemoteDataSource.getEvents().toList()
+        //then
+        assertEquals(emits, listOf(ResultState.Progress(), ResultState.Success(mockEvents)))
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test(expected = RuntimeException::class)
+    fun testFailApiResponse() = runBlocking {
+        //given
+        val exception = Throwable("Exception in service call")
+        whenever(api.getEvents(BuildConfig.API_KEY)).thenThrow(exception)
+        //when
+        val emits  = eventListRemoteDataSource.getEvents().toList()
+        //then
+        assertEquals(emits, listOf(ResultState.Progress(), ResultState.Failure<Events>(exception)))
+    }
 }
